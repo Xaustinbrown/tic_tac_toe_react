38cb51d53377123152c802577d8cf67eea3e55f5
Xavier Brown <xavieraustin1990@gmail>
2023-11-10 17:13:32 -0500

    tic-tac-toe

---
+++ src/Square.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Square = (props) => {
+
+  const handleClick = (event) => {
+    if (!props.squareValue){
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "0");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
+    }
+  }
+
+  return(
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+      {console.log(props.squares, props.player)}
+  </div>
+  )
+
+}
+
+export default Square;
